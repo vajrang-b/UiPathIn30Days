254b93428267dca3602e16ae23824941180eb0cc
Vajrang <vajrang@outlook.com>
2023-09-15 03:45:16 -0400

    reframework

--- Scripts/GenerateGptResponse.ps1
+++ Scripts/GenerateGptResponse.ps1
@@ -14,9 +14,9 @@ function GenerateGptResponse {
     }
     
     # Check if the length of $errorDetails is greater than 20,000 characters
-    if ($errorDetails.Length -gt 20000) {
+    if ($errorDetails.Length -gt 19000) {
         # Trim $errorDetails to the first 20,000 characters
-        $errorDetails = $errorDetails.Substring(0, 20000)
+        $errorDetails = $errorDetails.Substring(0, 19000)
     }
     #"content" = "you are worlds best code validator generate simple summary of errors to developer on correcting the error in english language, take influence of ironman movie dialouges in while creating, group together based on file names. with funny tone in each line,  it must be posted to github comment for pr "
 
