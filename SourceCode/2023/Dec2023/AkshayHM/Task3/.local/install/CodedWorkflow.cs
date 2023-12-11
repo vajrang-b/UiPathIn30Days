29efebef37c7b19795cca02265a1c25aaede8419
Akshay H Mulky <akshaymulky5211@gmail.com>
2023-12-11 19:21:54 +0530

    Task(5,6,7,8,9,10,11,12) Completed
    Added Switch to Task10 and While to Task12

--- SourceCode/2023/Dec2023/AkshayHM/Task3/.local/install/CodedWorkflow.cs
+++ SourceCode/2023/Dec2023/AkshayHM/Task3/.local/install/CodedWorkflow.cs
@@ -20,7 +20,7 @@ namespace Task3
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService)};
+            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
