0c32be838e0dc706debba132b9ee07ff43d4686b
Chandrika-97 <153043503+Chandrika-97@users.noreply.github.com>
2023-12-14 14:16:22 +0530

    task-14,task-15,task-16

--- SourceCode/2023/Dec2023/chandrika/Task-14/.local/install/CodedWorkflow.cs
+++ SourceCode/2023/Dec2023/chandrika/Task-14/.local/install/CodedWorkflow.cs
@@ -13,13 +13,13 @@ using UiPath.UIAutomationNext.API.Contracts;
 using UiPath.UIAutomationNext.API.Models;
 using UiPath.UIAutomationNext.Enums;
 
-namespace Task12
+namespace Task14
 {
     public partial class CodedWorkflow : CodedWorkflowBase
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
