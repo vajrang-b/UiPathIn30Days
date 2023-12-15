1cb6c5cd0fe5c4982f5072305a3deb8ccf3bcb40
Nazeernrb <146899432+Nazeernrb@users.noreply.github.com>
2023-12-15 09:22:19 +0530

    ReFrame Work 14/12/23
    File Name - Analyze The ReFrame Work
    ReFrame work Sample Build Data Table and add to Queues. Exception Checking The Retry Number, Consecuency Numbers

--- SourceCode/2023/Dec2023/Nazeer/Analyze the RE-Framework/.local/install/CodedWorkflow.cs
+++ SourceCode/2023/Dec2023/Nazeer/Analyze the RE-Framework/.local/install/CodedWorkflow.cs
@@ -20,7 +20,7 @@ namespace AnalyzetheREFramework
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService)};
+            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
