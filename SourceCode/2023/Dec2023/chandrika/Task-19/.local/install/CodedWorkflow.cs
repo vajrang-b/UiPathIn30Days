8c757018e1f3380e9befd120fbabff6760c73455
Chandrika-97 <153043503+Chandrika-97@users.noreply.github.com>
2023-12-18 14:57:00 +0530

    Task-19 ,task-20

--- SourceCode/2023/Dec2023/chandrika/Task-19/.local/install/CodedWorkflow.cs
+++ SourceCode/2023/Dec2023/chandrika/Task-19/.local/install/CodedWorkflow.cs
@@ -19,7 +19,7 @@ namespace Task19
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
