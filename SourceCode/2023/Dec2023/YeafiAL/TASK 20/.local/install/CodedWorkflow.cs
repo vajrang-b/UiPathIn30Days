d1e0f7de157d151c012ed6466a40685a21c9d946
yeafi-al <yeafiawal.ya@gmail.com>
2023-12-18 18:12:20 +0600

    TASK 20
    Read the excel (consists of Lower Letters), and type into website (Vajrang.org - Uppercase Task) and get the result and write into one excel

--- SourceCode/2023/Dec2023/YeafiAL/TASK 20/.local/install/CodedWorkflow.cs
+++ SourceCode/2023/Dec2023/YeafiAL/TASK 20/.local/install/CodedWorkflow.cs
@@ -20,7 +20,7 @@ namespace TASK20
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
