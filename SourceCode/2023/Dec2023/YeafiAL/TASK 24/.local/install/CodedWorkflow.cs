6ecefd53e704c11fe5cdae3ae33a183d846ed4ff
yeafi-al <yeafiawal.ya@gmail.com>
2023-12-21 20:24:05 +0600

    TASK 24
    Assign input value (eg: UiPath Studio) , and replace the word with any other word by using Replace function	Text Activities manipulation

--- SourceCode/2023/Dec2023/YeafiAL/TASK 24/.local/install/CodedWorkflow.cs
+++ SourceCode/2023/Dec2023/YeafiAL/TASK 24/.local/install/CodedWorkflow.cs
@@ -14,13 +14,13 @@ using UiPath.UIAutomationNext.API.Models;
 using UiPath.UIAutomationNext.Enums;
 using UiPath.CodedWorkflows.DescriptorIntegration;
 
-namespace Task_24
+namespace TASK24
 {
     public partial class CodedWorkflow : CodedWorkflowBase
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
@@ -31,14 +31,14 @@ namespace Task_24
     }
 }
 
-namespace Task_24.ObjectRepository
+namespace TASK24.ObjectRepository
 {
     public static class Descriptors
     {
     }
 }
 
-namespace Task_24._Implementation
+namespace TASK24._Implementation
 {
     internal class ScreenDescriptorDefinition : IScreenDescriptorDefinition
     {
